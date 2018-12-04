d30bade23a1c4eef4c1b2255a73039ccd3d7b871
Devon Thomassen <39370515+DevonThomassen@users.noreply.github.com>
2018-12-04 16:48:47 +0100

    Update Overview.jsx

--- front-end/src/Components/Overview.jsx
+++ front-end/src/Components/Overview.jsx
@@ -3,35 +3,33 @@ import React, {Component} from 'react';
 class Overview extends Component {
   render() {
     return (
-      <React.Fragment>
-        <div className='results_container'>
-          <table>
-            <thead>
+      <div className='results_container'>
+        <table>
+          <thead>
+          <tr>
+            <th colSpan="2">Results</th>
+          </tr>
+          </thead>
+          <tbody>
             <tr>
-              <th colSpan="2">Results</th>
+              <td>Url</td>
+              <td>{this.props.data.url}</td>
             </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Url</td>
-                <td>{this.props.data.url}</td>
-              </tr>
-              <tr>
-                <td>Time</td>
-                <td>{this.props.data.time}ms</td>
-              </tr>
-              <tr>
-                <td>Pages</td>
-                <td>{this.props.data.results}</td>
-              </tr>
-              <tr>
-                <td>Errors</td>
-                <td>{this.props.data.errors}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </React.Fragment>
+            <tr>
+              <td>Time</td>
+              <td>{this.props.data.time}ms</td>
+            </tr>
+            <tr>
+              <td>Pages</td>
+              <td>{this.props.data.results}</td>
+            </tr>
+            <tr>
+              <td>Errors</td>
+              <td>{this.props.data.errors}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
