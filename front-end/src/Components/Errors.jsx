e1c0162a6a63681f0ac3b77b0f4c8673441fc690
Devon Thomassen <39370515+DevonThomassen@users.noreply.github.com>
2018-12-04 16:46:08 +0100

    Create Errors.jsx

---
+++ front-end/src/Components/Errors.jsx
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import Error from "./Error";
+
+class Errors extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="clearfix"></div>
+        <div className='output_container'>
+          <h3>Errors</h3>
+          <table id='errors'>
+            <thead>
+            <tr>
+              <th>#</th>
+              <th>ERROR</th>
+            </tr>
+            </thead>
+            <tbody>
+            {this.props.data.map((result, i) => {
+              return <Error data={result} num={i + 1} key={i}/>;
+            })}
+            </tbody>
+          </table>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Errors;
