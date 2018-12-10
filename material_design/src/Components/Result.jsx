4b74470ecafe51dbb81c4f067cceb0b04a9c0828
Devon Thomassen <39370515+DevonThomassen@users.noreply.github.com>
2018-12-10 17:43:16 +0100

    Update Result.jsx

--- material_design/src/Components/Result.jsx
+++ material_design/src/Components/Result.jsx
@@ -1,32 +1,23 @@
 import React from 'react';
-import TableRow from "@material-ui/core/TableRow";
 import {withStyles} from "@material-ui/core";
+import TableRow from '@material-ui/core/TableRow';
 import TableCell from "@material-ui/core/TableCell";
 
-
 const CustomTableCell = withStyles(theme => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  body: {
-    fontSize: 14,
-  },
+
 }))(TableCell);
 
 const styles = theme => ({
-  root: {
-    width: '100%',
-    marginTop: theme.spacing.unit * 3,
-    overflowX: 'auto',
+  row:{
+    '&:nth-child(odd)': {
+      backgroundColor: `#a0a0a0`
+    }
   },
-  table: {
-    minWidth: 700,
-  },
-  row: {
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.background.default,
-    },
+  index: {
+
+    width: `50px`,
+    paddingLeft: `0`,
+    paddingRight: `0`,
   },
 });
 
@@ -34,11 +25,11 @@ function Result(props) {
   const {classes} = props;
   return (
     <React.Fragment>
-      <TableRow>
-        <CustomTableCell>{props.data.num}</CustomTableCell>
+      <TableRow className={classes.row}>
+        <CustomTableCell className={classes.index}>{props.data.num}</CustomTableCell>
         <CustomTableCell>{props.data.url}</CustomTableCell>
         <CustomTableCell>{props.data.statusCode}</CustomTableCell>
-        <CustomTableCell>{props.data.milliseconds}</CustomTableCell>
+        <CustomTableCell className='ms'>{props.data.milliseconds}</CustomTableCell>
       </TableRow>
     </React.Fragment>
   )
