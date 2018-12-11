3a676d8b641dfaba0bf2ba3292429ba7b7f89cac
Devon Thomassen <39370515+DevonThomassen@users.noreply.github.com>
2018-12-11 17:36:26 +0100

    Update Result.jsx

--- front-end/src/Components/Result.jsx
+++ front-end/src/Components/Result.jsx
@@ -1,13 +1,26 @@
 import React, {Component} from 'react';
 
 class Result extends Component {
+  b64EncodeUnicode(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+      function toSolidBytes(match, p1) {
+        return String.fromCharCode('0x' + p1);
+      }));
+  }
+
   render() {
     return (
       <React.Fragment>
-        <a className="row" title={this.props.data.url} href={"data:text/plain;base64," + btoa(this.props.data.body)} target={"_blank"}>
+        <a className="row"
+           title={this.props.data.url}
+           href={this.props.data.body === undefined ? '' 
+             : 'data:text/plain;base64,' + this.b64EncodeUnicode(this.props.data.body)}
+           target={this.props.data.body === undefined ? '' : '_blank'}>
           <div className="cell">{this.props.data.num}</div>
           <div className="cell">{this.props.data.url}</div>
-          <div className={"cell " + (this.props.data.statusCode === 200 ? 'good' : 'bad')}>{this.props.data.statusCode}</div>
+          <div
+            className={"cell " + (Math.floor(this.props.data.statusCode / 100) === 2 ? 'good' : 'bad')}>
+            {this.props.data.statusCode}</div>
           <div className="cell ms">{this.props.data.milliseconds}</div>
         </a>
       </React.Fragment>
