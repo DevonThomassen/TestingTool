a4f41906480134eba6a305301e54d853cf7d730b
Devon Thomassen <39370515+DevonThomassen@users.noreply.github.com>
2018-12-11 17:29:50 +0100

    Update Result.jsx

--- material_design/src/Components/Result.jsx
+++ material_design/src/Components/Result.jsx
@@ -1,38 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {withStyles} from "@material-ui/core";
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from "@material-ui/core/TableCell";
 
-const CustomTableCell = withStyles(theme => ({
+const CustomTableCell = TableCell;
 
-}))(TableCell);
-
-const styles = theme => ({
-  row:{
-    '&:nth-child(odd)': {
-      backgroundColor: `#a0a0a0`
+const styles = () => ({
+  row: {
+    '&:nth-child(even)': {
+      backgroundColor: `#dbdbdb`
     }
   },
   index: {
-
     width: `50px`,
     paddingLeft: `0`,
     paddingRight: `0`,
   },
+  item: {
+    width: `50px`,
+    padding: `4px 24px 4px 24px`,
+    textAlign: `center`,
+
+    '&:last-of-type': {
+      textAlign: `right`,
+    }
+  }
 });
 
-function Result(props) {
-  const {classes} = props;
-  return (
-    <React.Fragment>
-      <TableRow className={classes.row}>
-        <CustomTableCell className={classes.index}>{props.data.num}</CustomTableCell>
-        <CustomTableCell>{props.data.url}</CustomTableCell>
-        <CustomTableCell>{props.data.statusCode}</CustomTableCell>
-        <CustomTableCell className='ms'>{props.data.milliseconds}</CustomTableCell>
-      </TableRow>
-    </React.Fragment>
-  )
+
+class Result extends Component {
+  
+  b64EncodeUnicode(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+      function toSolidBytes(match, p1) {
+        return String.fromCharCode('0x' + p1);
+      }));
+  }
+
+  render() {
+    const {classes} = this.props;
+    return (
+      <React.Fragment>
+        <TableRow className={`${classes.row} ${this.props.data.body === undefined ? 'image' : 'link'}`}
+                  onClick={() => this.props.data.body === undefined ? ''
+                    : window.open('data:text/plain;base64,' + this.b64EncodeUnicode(this.props.data.body))}>
+          <CustomTableCell className={classes.index}>{this.props.data.num}</CustomTableCell>
+          <CustomTableCell>{this.props.data.url}</CustomTableCell>
+          <CustomTableCell
+            className={`${classes.item} ${Math.floor(this.props.data.statusCode / 100) === 2 ? 'good' : 'bad'}`}>{this.props.data.statusCode}</CustomTableCell>
+          <CustomTableCell className={'ms ' + classes.item}>{this.props.data.milliseconds}</CustomTableCell>
+        </TableRow>
+      </React.Fragment>
+    )
+  }
 }
 
 export default withStyles(styles)(Result);
