a777b2815e1a450f9dd5cc57266b34aab3ce676d
Devon Thomassen <39370515+DevonThomassen@users.noreply.github.com>
2018-12-21 17:12:48 +0100

    Added sort functions

--- front-end typescript/src/components/Result.tsx
+++ front-end typescript/src/components/Result.tsx
@@ -1,25 +1,84 @@
 import * as React from 'react';
 import CResult from '../classes/Result';
-
+import Result from './Result';
 
 interface Props {
-  result: CResult;
+  results: CResult[];
 }
 
-class Result extends React.Component<Props, {}> {
+interface State {
+  orderProperty: string;
+  orderAscending: boolean;
+}
+
+class Results extends React.Component<Props, State> {
+
+  public state: State = {
+    orderProperty: 'num',
+    orderAscending: true
+  }
+
+  toggleOrderProperty = (propertyName: string) => {
+    if (this.state.orderProperty === propertyName) {
+      this.setState({
+        orderProperty: this.state.orderProperty,
+        orderAscending: !this.state.orderAscending
+      });
+    } else {
+      this.setState({
+        orderProperty: propertyName,
+        orderAscending: true
+      });
+    }
+  };
+
+  toggleSortByNum = () => {
+    this.toggleOrderProperty('num');
+  };
+
+  toggleSortByUrl = () => {
+    this.toggleOrderProperty('url');
+  };
+
+  toggleSortByCode = () => {
+    this.toggleOrderProperty('statusCode');
+  };
+
+  toggleSortByTime = () => {
+    this.toggleOrderProperty('milliseconds');
+  };
+
+  orderFn = (resultA: CResult, resultB: CResult) : number => {
+    const order = this.state.orderAscending ? 1 : -1
+    
+    return resultA[this.state.orderProperty] > resultB[this.state.orderProperty] ? order : -order;
+  };
+
   public render() {
     return (
       <React.Fragment>
-        <div className="row">
-          <div className="cell">{this.props.result.num}</div>
-          <div className="cell">{this.props.result.url}</div>
-          <div className={"cell " + (Math.floor(this.props.result.statusCode / 100) === 2 ? 'good' : 'bad')}>
-            {this.props.result.statusCode}</div>
-          <div className="cell ms">{this.props.result.milliseconds}</div>
+        <div className="clearfix" />
+        <div className='output_container'>
+          <h3>Results</h3>
+          <div id='results' className={"table " + (this.state.orderAscending ? 'ordered-ascending' : 'ordered-descending')}>
+            <div className="row">
+              <div className={"head order " + (this.state.orderProperty !== 'num' ? '' : 'selected')}
+                onClick={this.toggleSortByNum}>#</div>
+              <div className={"head order " + (this.state.orderProperty !== 'url' ? '' : 'selected')}
+                onClick={this.toggleSortByUrl}>URL</div>
+              <div className={"head order " + (this.state.orderProperty !== 'statusCode' ? '' : 'selected')}
+                onClick={this.toggleSortByCode}>CODE</div>
+              <div className={"head order " + (this.state.orderProperty !== 'milliseconds' ? '' : 'selected')}
+                onClick={this.toggleSortByTime}>TIME</div>
+            </div>
+            {this.props.results.sort(this.orderFn).map((result: CResult) => {
+              return <Result result={result} key={'Result' + result.num} />;
+            })}
+          </div>
         </div>
       </React.Fragment>
     );
   }
 }
 
-export default Result;
+export default Results;
